0770cbcd70bb33e6da0a1b0d43f1d41a00809ab1
Tu Ngo <tu.ngo27@student.passerellesnumeriques.org>
2026-07-16 09:03:25 +0700

    chore: remove unused and redundant files

--- backend/clearOrders.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  await prisma.transaction.deleteMany();
-  await prisma.orderItem.deleteMany();
-  await prisma.order.deleteMany();
-  console.log("Deleted all orders");
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
